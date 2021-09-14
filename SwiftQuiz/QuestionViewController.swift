cb3abf1c095a44df01acc214e19a961a445883d8
AlexandrMeyer <alexandrmeyerr@gmail.com>
2021-09-14 13:38:45 +0300

    add QuestionVC

---
+++ SwiftQuiz/QuestionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionViewController.swift
+//  SwiftQuiz
+//
+//  Created by Александр on 14.09.21.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    
+    
+    private let questionIndex = 0
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
